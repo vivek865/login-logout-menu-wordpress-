367018a3212c410840641df043b9ec7b36a1a9a1
vivek865 <kumarvivek865@gmail.com>
2016-05-09 08:24:34 +0530

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,15 @@
+        <?php
+$current_user = wp_get_current_user();
+if ( 0 == $current_user->ID ) {
+   ?>
+	<li><a href="#nav-login-dialog" data-effect="mfp-move-from-top" class="popup-text"> Sign In <?php
+} else {
+    echo '<li><a href="'. wp_logout_url( $redirect ). '"><span >' . $current_user->user_login . '</span> signout ';
+}
+?>
+						  
+						
+						
+						
+						</a>
+                        </li>
